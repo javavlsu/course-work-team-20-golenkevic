784dee00d817d3ccc8171ca8fdab354bc73d671c
Denis <denisonaylol@gmail.com>
2026-04-26 18:21:43 +0300

    Мега-меню категорий на главной странице
    - При наведении на категорию (Одежда, Обувь и т.д.) выпадает блок 760px
    - В мега-меню две колонки: подборки + крупное фото
    - Подборки: Новинки, Новые, Б/у в отличном состоянии, Винтаж, кнопка Смотреть все
    - Фото берётся из photoformain по индексу категории
    - IndexController передаёт список категорий
    - Hover через group/group-hover (Tailwind), плавное появление
    - На мобильных мега-меню скрыто, остаётся простая лента

--- marketplace/src/main/java/ru/vlsu/marketplace/controllers/IndexController.java
+++ marketplace/src/main/java/ru/vlsu/marketplace/controllers/IndexController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.vlsu.marketplace.repositories.CategoryRepository;
 import ru.vlsu.marketplace.services.ProductService;
 
 @Controller
@@ -11,6 +12,7 @@ import ru.vlsu.marketplace.services.ProductService;
 public class IndexController {
 
     private final ProductService productService;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -18,6 +20,7 @@ public class IndexController {
                 .map(productService::convertToDto).toList());
         model.addAttribute("popular", productService.getPopular(8).stream()
                 .map(productService::convertToDto).toList());
+        model.addAttribute("categories", categoryRepository.findAll());
         return "index";
     }
 }
